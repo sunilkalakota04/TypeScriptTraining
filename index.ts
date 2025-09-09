1ad4fcd71a9ce2b23fb4b6fa91748d8d6fc8be43
sunilkalakota04 <sunillvsu@live.com>
2025-09-09 13:05:55 +0530

    Day2 practice and jest framework creation.

--- index.ts
+++ index.ts
@@ -5,7 +5,77 @@ let isActiveValue:boolean = true
 let list:number[] = [1,2,3,4,5]
 
 
-console.log(`sum of a and b: ${a+b}`);
-console.log(`Welcome message: '${message}`);
-console.log(`User active?: '${isActiveValue}`);
-console.log(`lists: '${list}`);
+// console.log(`sum of a and b: ${a+b}`);
+// console.log(`Welcome message: '${message}`);
+// console.log(`User active?: '${isActiveValue}`);
+// console.log(`lists: '${list}`);
+
+
+const point = {a:1, b:2, c:3}
+let x = point.a
+let y = point.b
+
+// console.log(x,y)
+
+const user = {
+    id: 1,
+    name: "sunil",
+    address: {
+        street: 'Rd.#12',
+        city: 'Hyderabad',
+        state: 'Telangana',
+        country: 'India'
+    }
+}
+
+const {address: {city},...others} = user
+console.log(city, others)
+
+function sum(...numbers:number[]):number{
+    let result = 0;
+
+    for (const num of numbers){
+
+        result += num
+    }
+    return result;
+}
+
+// console.log(sum(2,3,4,5))
+
+let response: string | number = '20'
+
+if(typeof response === "string"){
+    console.log((response as string).toUpperCase());
+} else {
+
+    console.log((response as number));
+}
+
+type cat = {
+    meow:() => void;
+}
+
+type dog = {
+    woff:() => void;
+}
+
+function iscat(animal: cat|dog): animal is cat {
+    return(animal as cat).meow !==undefined
+}
+
+function makeSound(animal:cat|dog){
+    if(iscat(animal)){
+        animal.meow
+    }else {
+        animal.woff
+    }
+}
+
+const pet1:cat = {
+    meow:() => console.log('Meow!!')
+} 
+
+const pet2:dog = {
+    woff:() => console.log('Woff!')
+}
---
+++ sum.test.ts
@@ -0,0 +1,6 @@
+import {sum} from './sum'
+
+test('test add function',()=>{
+
+    expect(sum(1,2)).toBe(3)
+})
